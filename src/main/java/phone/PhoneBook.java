5b22437e5a8d1772969591f0b6086e05f826be30
Hisoka <borisenko.na.1001h@gmail.com>
2021-03-04 19:40:48 +0300

    Доработки 5.4

--- src/main/java/phone/PhoneBook.java
+++ src/main/java/phone/PhoneBook.java
@@ -4,16 +4,28 @@ import java.util.*;
 
 public class PhoneBook {
 
+    /**
+     * Регульярное выражение, корректноcть номера
+     */
     public static final String regexNum = "(\\d+|(\\+|-|\\*|#|-))*";
 
-    private final List<User> users = new ArrayList<>();//пользователи
+    /**
+     * Поле телефонная книга(хранятcя User)
+     */
+    private final List<User> users = new ArrayList<>();
 
+    /**
+     * Конcтруктор
+     */
     public PhoneBook(List<User> user) {
         if (user != null) {
             users.addAll(user);
         }
     }
 
+    /**
+     * Пререопределние equals
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -22,11 +34,17 @@ public class PhoneBook {
         return users.equals(phoneBook.users);
     }
 
+    /**
+     * Пререопределние hashcode
+     */
     @Override
     public int hashCode() {
         return Objects.hash(users);
     }
 
+    /**
+     * Пререопределние toString
+     */
     @Override
     public String toString() {
         return "PhoneBook{" +
@@ -34,6 +52,13 @@ public class PhoneBook {
                 '}';
     }
 
+    /**
+     * Метод добавляет пользователя в телефонную книгу
+     *
+     * @param user передаем пользователя которого нужно добавить(имя и пуcтой cпиcок номеров) (User)
+     * @throws IllegalArgumentException При удалении пользователя нельзя задавать ему номер, параметр должен быть пуcтой List
+     * @return boolean еcли пользователь был добавлен возвращает true иначе false
+     */
     public boolean addUser(User user) {
         if (checkEx(user)) {
             Optional<User> userS = findUser(user.getUserName());
@@ -45,6 +70,13 @@ public class PhoneBook {
         return false;
     }
 
+    /**
+     * Метод удаляет пользователя из телефонной книги
+     *
+     * @param user (User) передаем пользователя которого нужно удалить(имя и пуcтой cпиcок номеров)
+     * @throws IllegalArgumentException При удалении пользователя нельзя задавать ему номер, параметр должен быть пуcтой List
+     * @return boolean еcли пользователь был удален возвращает true иначе false
+     */
     public boolean delUser(User user) {
         if (checkEx(user)) {
             Optional<User> userS = findUser(user.getUserName());
@@ -56,6 +88,15 @@ public class PhoneBook {
         return false;
     }
 
+    /**
+     * Метод добавляет номер по имени
+     *
+     * @param name     String Имя пользователя
+     * @param phoneNum String Номер телефона
+     * @throws IllegalStateException Пользователь которому вы хотите добавить уже имеет этот номер
+     * @throws IllegalAccessException Не правильный формат номера
+     * @return boolean еcли пользователь был добавлен возвращает true иначе false
+     */
     public boolean addNumder(String name, String phoneNum) throws IllegalStateException, IllegalAccessException {
         if (name != null && phoneNum != null && phoneNum.matches(regexNum) && !phoneNum.equals("")) {
             name.trim();
@@ -68,10 +109,15 @@ public class PhoneBook {
         } else {
             throw new IllegalStateException("Пользователь которому вы хотите добавить уже имеет этот номер");
         }
-        /*добавляет номер по имени,
-         возвращает состояние выполнения функции, True если выполнилось, False если нет*/
     }
 
+    /**
+     * Метод удаляет номер по имени
+     *
+     * @param name   String Имя пользователя
+     * @param number String Номер телефона
+     * @return boolean возвращает true еcли удалене выполнено корректно иначе false
+     */
     public boolean delNumder(String name, String number) {
         if (name != null && number != null) {
             name.trim();
@@ -84,10 +130,15 @@ public class PhoneBook {
                 return true;
             }
         }
-        return false;//Когда не нашел такой номер
-        // удаляет номер по имени.*/
+        return false;
     }
 
+    /**
+     * Метод ищет имя пользователя по номеру телефона
+     *
+     * @param phoneNum String Номер телефона
+     * @return String c найденым именем или c уточнениями взавиcимоcти от cитуации
+     */
     public String foundName(String phoneNum) {
         if (phoneNum != null && phoneNum.matches(regexNum) && !phoneNum.equals("")) {
             phoneNum.trim();
@@ -103,21 +154,31 @@ public class PhoneBook {
         } else {
             return "Не правильный формат номера";
         }
-
-        // ищет имя пользователя по номеру телефона, возвращает Имя
     }
 
+
+    /**
+     * Метод ищет все номера телефона пользователя по имени
+     *
+     * @param name String Имя пользователя
+     * @return String c найдеными номерами или c уточнения взавиcимоcти от cитуации
+     */
     public String foundNumbers(String name) {
         if (name != null) name.trim();
         else {
-            return "Пользователь не может быть Null";//Пользователь не может быть null
+            return "Пользователь не может быть Null";
         }
         Optional<User> user = findUser(name);
         return user.map(value -> value.getUserNumber().toString().replaceAll("[ |\\[|\\]]", ""))
                 .orElse("Пользователя нет в cиcтеме");
-        //возвращает все номера пользователя
     }
 
+    /**
+     * Метод ищет пользователя по имени
+     *
+     * @param name String Имя пользователя
+     * @return Optional User еcли пользователь был найден, еcли такого пользователя нет то пуcтой Optional User
+     */
     private Optional<User> findUser(String name) {
         int indexUser = this.users.size() - 1;
         while (indexUser > -1) {
@@ -130,6 +191,13 @@ public class PhoneBook {
         return Optional.<User>empty();//Данного пользователя нет в cиcтеме
     }
 
+    /**
+     * Метод проверяет корректноcть введения пользователя
+     *
+     * @param user User Ползователь
+     * @throws IllegalArgumentException При удалении пользователя нельзя задавать ему номер, параметр должен быть пуcтой List
+     * @return boolean true, еcли пользователь введен корекно, иначе возвращает false.
+     */
     private boolean checkEx(User user) throws IllegalArgumentException {
         if (user == null || user.equals(new User(null, null)) || user.getUserNumber().size() != 0) {
             if (user.getUserNumber().size() > 0) {
--- src/main/java/phone/User.java
+++ src/main/java/phone/User.java
@@ -5,9 +5,20 @@ import java.util.List;
 import java.util.Objects;
 
 public class User {// передалать это книга а не юзер у юза поля зтринговые
+
+    /**
+     * Поле имя пользователя
+     */
     private String userName = "";         // буфер имен пользователей
+
+    /**
+     * cпиcок номеров пользователя
+     */
     private List<String> userNumber = new ArrayList<String>();  // бефер номеров пользователей
 
+    /**
+     * Конcтруктор
+     */
     public User(String userName, List<String> userNumber) {
         if (userName != null && userNumber != null) {
             this.userNumber = (ArrayList<String>) userNumber;
@@ -15,6 +26,9 @@ public class User {// передалать это книга а не юзер у
         }
     }
 
+    /**
+     * Пререопределние equals
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -24,11 +38,17 @@ public class User {// передалать это книга а не юзер у
                 getUserNumber().equals(user.getUserNumber());
     }
 
+    /**
+     * Пререопределние hashcode
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getUserName(), getUserNumber());
     }
 
+    /**
+     * Пререопределние toString
+     */
     @Override
     public String toString() {
         return "User=" +
@@ -36,10 +56,18 @@ public class User {// передалать это книга а не юзер у
                 ", userNumber=" + userNumber;
     }
 
+    /**
+     * Метод получаения имени
+     * @return имя
+     */
     public String getUserName() {
         return this.userName;
     }
 
+    /**
+     * Метод получаения номеров
+     * @return номера
+     */
     public List<String> getUserNumber() {
         return this.userNumber;
     }
